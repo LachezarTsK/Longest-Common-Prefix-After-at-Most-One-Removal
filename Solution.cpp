6cfbc2d11c6103004fefcdad966453c24fb3227b
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-24 20:32:26 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,31 @@
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+
+public:
+    int longestCommonPrefix(const string& source, const string& target) const {
+        int maxPrefixLength = min(source.length(), target.length());
+        int checkLengthSource = min(source.length(), maxPrefixLength + 1);
+
+        int indexSource = 0;
+        int indexTarget = 0;
+        int longestCommonPrefix = 0;
+
+        while (indexSource < checkLengthSource && indexTarget < maxPrefixLength) {
+            if (source[indexSource] == target[indexTarget]) {
+                ++indexTarget;
+                ++longestCommonPrefix;
+            }
+            else if (indexSource > indexTarget) {
+                break;
+            }
+            ++indexSource;
+        }
+
+        return longestCommonPrefix;
+    }
+};
