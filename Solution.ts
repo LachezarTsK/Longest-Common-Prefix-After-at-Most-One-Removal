96352548d3d31c1a5901004618ebb2b7160df61d
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-24 20:30:13 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,21 @@
+
+function longestCommonPrefix(source: string, target: string): number {
+    const maxPrefixLength = Math.min(source.length, target.length);
+    const checkLengthSource = Math.min(source.length, maxPrefixLength + 1);
+
+    let indexSource = 0;
+    let indexTarget = 0;
+    let longestCommonPrefix = 0;
+
+    while (indexSource < checkLengthSource && indexTarget < maxPrefixLength) {
+        if (source[indexSource] === target[indexTarget]) {
+            ++indexTarget;
+            ++longestCommonPrefix;
+        } else if (indexSource > indexTarget) {
+            break;
+        }
+        ++indexSource;
+    }
+
+    return longestCommonPrefix;
+};
